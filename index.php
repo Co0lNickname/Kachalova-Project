3f94c5ad25817feab56166eb796fffe5cedb37a3
DocLivsey <doclivsey@github.com>
2025-01-16 20:26:11 +0300

    feat: place selecting user from database into try-catch block

--- index.php
+++ index.php
@@ -53,10 +53,16 @@
     <ol>
       <?php
         $pdo = require __DIR__ . '/db/config/db.php';
-        $stmt = $pdo->prepare("SELECT * FROM Client WHERE ClientID = :id");
-        $stmt->execute(['id' => $_SESSION['client_id']]);
-        $client = $stmt->fetch();
-        $is_admin = $client['IsAdmin'];
+        $stmt = $pdo->prepare("SELECT * FROM Client WHERE ClientID = :id"); 
+        try {
+          $stmt->execute(['id' => $_SESSION['client_id']]);
+          $client = $stmt->fetch();
+          $is_admin = $client['IsAdmin'];
+        }
+        catch (Exception $exception) {
+          $is_admin = false;
+        }
+        
         if ($is_admin) {
             $pages['admin'] = './../admin/index.php';
         }
