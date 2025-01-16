5c1c0ab0f756c445e02297a94f47afbf26ecf1f1
DocLivsey <doclivsey@github.com>
2025-01-16 21:41:31 +0300

    feat: change writing user's data into base

--- src/admin/signup.php
+++ src/admin/signup.php
@@ -3,13 +3,13 @@ $pdo = require __DIR__ . '/../../db/config/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
+    $username = $_POST['username'];
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $wishes = $_POST['wishes'] ?? null;
 
-    $stmt = $pdo->prepare("INSERT INTO Client (Name, Email, Password, Wishes, IsAdmin) VALUES (?, ?, ?, ?, ?)");
+    $stmt = $pdo->prepare("INSERT INTO Client (Name, Username, Email, Password, IsAdmin, IsActive, IsBlocked) VALUES (?, ?, ?, ?, ?, ?, ?)");
     try {
-        $stmt->execute([$name, $email, $password, $wishes, 0]);
+        $stmt->execute([$name, $username, $email, $password, 0, 1, 0]);
         echo "
             <div>Signup successful.</div>
             <div>Back to <a href='/index.php'>main page</a></div>
