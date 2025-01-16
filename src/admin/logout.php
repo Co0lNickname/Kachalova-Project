b4817758cf71bc8e58c36ffd32b9053b369b7fda
DocLivsey <doclivsey@github.com>
2025-01-16 22:18:10 +0300

    feat: added changing "IsActive" field

--- src/admin/logout.php
+++ src/admin/logout.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
 session_destroy();
+$userID = $_SESSION['client_id'];
+$stmt = $pdo->prepare("UPDATE User IsActive = 0 WHERE UserID = ?");
+$stmt->execute([$userID]);
+$stmt->fetch();
 header("Location: login.html");
 exit;
