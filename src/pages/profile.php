592e3db74d7b026d28e2bff721e99d592be7ad52
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-19 21:35:05 +0300

    feat: added redirection for changing profile

--- src/pages/profile.php
+++ src/pages/profile.php
@@ -73,7 +73,7 @@
     </div>
 </div>
 <div class="change-data">
-    <a href="/index.php">
+    <a href="/src/pages/changeProfileFrom.html">
         <button class="pretty-button">Change data</button>
     </a>
 </div>
