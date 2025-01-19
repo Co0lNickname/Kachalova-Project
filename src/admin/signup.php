c89797dd807da263a83d99d46161a56447b67129
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-19 21:23:05 +0300

    feat: temporally changed to old version without profile image

--- src/admin/signup.php
+++ src/admin/signup.php
@@ -8,14 +8,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
     $stmtUser = $pdo->prepare("INSERT INTO User (Name, Username, Email, Password, IsAdmin, IsActive, IsBlocked) VALUES (?, ?, ?, ?, ?, ?, ?)");
-	$stmtPicture = $pdo->prepare("INSERT INTO ProfilePictures (ProfileImage, MimeType) VALUES (?, ?)");
-	$stmtRelation = $pdo->prepare("INSERT INTO UsersImages (UserID, ImageID) VALUES (?, ?)");
+//	$stmtPicture = $pdo->prepare("INSERT INTO ProfilePictures (ProfileImage, MimeType) VALUES (?, ?)");
+//	$stmtRelation = $pdo->prepare("INSERT INTO UsersImages (UserID, ImageID) VALUES (?, ?)");
     try {
         $stmtUser->execute([$name, $username, $email, $password, 0, 1, 0]);
-		$user = $stmtUser->fetch();
-		$stmtPicture->execute([null, null]);
-		$image = $stmtPicture -> fetch();
-		$stmtRelation->execute([$user['UserID'], $image['ImageID']]);
+//		$user = $stmtUser->fetch();
+//		$stmtPicture->execute([null, null]);
+//		$image = $stmtPicture -> fetch();
+//		$stmtRelation->execute([$user['UserID'], $image['ImageID']]);
         echo "
             <div>Signup successful.</div>
             <div>Back to <a href='/index.php'>main page</a></div>
--- src/pages/profile.php
+++ src/pages/profile.php
@@ -9,13 +9,14 @@
     } else {
         $isLogin = true;
         $clientId = $_SESSION['client_id'];
-        $stmt = $pdo->prepare(
-                "SELECT Name, Username, Email, User.UserID, ProfileImage, MimeType
-                FROM User 
-                    JOIN UsersImages ON User.UserID = UsersImages.UserID 
-                    JOIN ProfilePictures on ProfilePictures.ImageID = UsersImages.ImageID
-                WHERE User.UserID = :id"
-        );
+//        $stmt = $pdo->prepare(
+//                "SELECT Name, Username, Email, User.UserID, ProfileImage, MimeType
+//                FROM User
+//                    JOIN UsersImages ON User.UserID = UsersImages.UserID
+//                    JOIN ProfilePictures on ProfilePictures.ImageID = UsersImages.ImageID
+//                WHERE User.UserID = :id"
+//        );
+		$stmt = $pdo->prepare("SELECT * FROM User WHERE UserID = :id");
         $stmt->execute(['id' => $clientId]);
         $client = $stmt -> fetch();
     }
@@ -23,8 +24,8 @@
     $name = $client != null ? $client['Name'] : 'Undefined';
     $userName = $client != null ? $client['Username'] : 'Undefined';
     $email = $client != null ? $client['Email'] : 'Undefined';
-    $profileImageBlob = $client != null ? $client['ProfileImage'] : 'Undefined';
-    $mimeType = $client != null ? $client['MimeType'] : 'Undefined';
+//    $profileImageBlob = $client != null ? $client['ProfileImage'] : 'Undefined';
+//    $mimeType = $client != null ? $client['MimeType'] : 'Undefined';
 
 //    $image = imagecreatefromstring($profileImageBlob);
 //
