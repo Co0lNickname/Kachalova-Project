30ef1a52c9171f32369c1033f507b268bdd015b3
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-19 21:57:48 +0300

    feat: added handlers for updating user's profile data

--- src/admin/editProfile.php
+++ src/admin/editProfile.php
@@ -2,7 +2,25 @@
 $pdo = require __DIR__ . '/../../db/config/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	$name = $_POST['name'];
-	$username = $_POST['username'];
-	$email = $_POST['email'];
+	if (isset($_SESSION['client_id'])) {
+		$userID = $_SESSION['client_id'];
+		$name = $_POST['name'];
+		$username = $_POST['username'];
+		$email = $_POST['email'];
+
+		$stmt = $pdo->prepare("UPDATE User SET Name = :name, Username = :username, Email = :email WHERE UserID = :id");
+		try {
+			$stmt->execute([
+				'name' => $name,
+				'username' => $username,
+				'email' => $email,
+				'id' => $userID
+			]);
+		} catch (PDOException $e) {
+			echo "Error: " . $e->getMessage();
+		}
+	} else {
+		echo 'You are not logged in';
+		echo '<a href="login.php">Login</a>';
+	}
 }
