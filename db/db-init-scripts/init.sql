fadb73511b6196cc37bbaa1c437eb9d08a27477f
DocLivsey <doclivsey@github.com>
2025-01-19 15:19:26 +0300

    feat: added new table for storing user's images and change constraints on FriendShip table

--- db/db-init-scripts/init.sql
+++ db/db-init-scripts/init.sql
@@ -3,14 +3,25 @@ CREATE TABLE IF NOT EXISTS User (
     Name VARCHAR(100),
     Username VARCHAR(100) UNIQUE,
     Email VARCHAR(100) UNIQUE,
-    ProfileImage BLOB,
     Password VARCHAR(100),
     IsAdmin BOOLEAN,
     IsActive BOOLEAN,
     IsBlocked BOOLEAN
 );
 CREATE TABLE IF NOT EXISTS Friendship (
-    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    UserID INT,
     FriendID INT,
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
     FOREIGN KEY (FriendID) REFERENCES User(UserID)
 );
+CREATE TABLE IF NOT EXISTS ProfilePictures (
+    ImageID INT AUTO_INCREMENT PRIMARY KEY,
+    ProfileImage BLOB,
+    MimeType VARCHAR(30)
+);
+CREATE TABLE IF NOT EXISTS UsersImages (
+    UserID INT,
+    ImageID INT,
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (ImageID) REFERENCES ProfilePictures(ImageID)
+)
