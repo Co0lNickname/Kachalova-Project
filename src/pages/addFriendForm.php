ae7f53d22641951332e201f8547b5dedf9720596
DocLivsey <doclivsey@github.com>
2025-01-19 23:53:47 +0300

    feat: create new form for adding friend

---
+++ src/pages/addFriendForm.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Make Friends!</title>
+</head>
+<body>
+    <div class="add-friend">
+        <label for="username">Enter your friend's username:</label>
+        <input type="text" id="username" name="username" required>
+        <br>
+        <div class="confirm-button">
+            <button class="pretty-button">Add Friend</button>
+        </div>
+    </div>
+</body>
+</html>
